9fc51e47e1d975fb9e4104072b9a7dfd31b96ee4
Thenmozhivijayan <thenmozhivijayan70@gmail.com>
2025-04-06 20:22:34 +0530

    s2

--- connection/connect.php
+++ connection/connect.php
@@ -1,18 +1,14 @@
 <?php
 
-$host     = "dpg-cvp91n24d50c73bqa53g-a"; // From Render
-$port     = "5432";
-$dbname   = "cdb_ce09";
-$user     = "cdb_ce09_user";
-$password = "W5JgrjD9Wao6PV0g2XwIvzCJYKed5czT";
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "code_camp_bd_fos";  
 
-// Build connection string
-$connStr = "host=$host port=$port dbname=$dbname user=$user password=$password";
 
-// Connect to PostgreSQL
-$db = pg_connect($connStr);
-
-if (!$db) {
-    die("PostgreSQL connection failed.");
+$db = mysqli_connect($servername, $username, $password, $dbname); 
+if (!$db) {      	
+    die("Connection failed: " . mysqli_connect_error());
 }
+
 ?>
